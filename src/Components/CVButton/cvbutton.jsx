e3cfafe6b7795d4e5e1c71bb557bb12e4ab45361
Eddie <eddie-tayeb@hotmail.fr>
2024-02-15 12:16:09 +0100

    Modification du fichier CV pour déploiement

--- src/Components/CVButton/cvbutton.jsx
+++ src/Components/CVButton/cvbutton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {FaAngleRight} from 'react-icons/fa';
 import {motion} from 'framer-motion';
 import './cvbutton.css';
-import CV from '../../../assets/CV/CV_EDDIE_GHALEM.PDF';
+import CV from '../../../assets/cv/cv_eddie_ghalem.pdf';
 
 const CVButton = () => {
     return (
